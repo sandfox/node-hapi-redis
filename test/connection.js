22420e565ec3ba72d8029e884a4a520b7dd8cb58
Ju Hae Lee <juhaelee@Jus-MacBook-Pro.local>
2015-06-25 10:39:19 -0700

    promise support

--- test/connection.js
+++ test/connection.js
@@ -2,21 +2,35 @@ var Hapi = require('hapi');
 var assert = require('assert');
 
 var redisPlugin = require('../');
+var server = null;
 
 describe('Hapi server', function() {
 
   //Need to test option parsing works... and so so much more
 
   it('should be able to register plugin default options', function(done) {
-    var server = new Hapi.Server();
+    server = new Hapi.Server();
     server.register({
       register: redisPlugin
     }, function () {
       assert(server.plugins['hapi-redis'].client,'no redis client was returned');
+
       server.plugins['hapi-redis'].client.ping(function(err, res){
         assert.equal(res, 'PONG', "didn't get a pong for our ping")
-        done()
-      })
+        done();
+      });
     });
+  });
+
+  it('should be able to use async functions', function(done){
+    server.plugins['hapi-redis'].client.pingAsync()
+      .then(function(res){
+        assert.equal(res, 'PONG', "didn't get a pong for our ping");
+        done();
+      })
+      .catch(function(e){
+        assert.ok(false, "Async didn't work");
+        done();
+      });
   });
 });
