35b445771b3e53b22b329a082fd8ffe78ebb4159
James Butler <james.butler@sandfox.co.uk>
2015-08-30 16:47:44 +0100

    fix eslint errors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ exports.register = function (server, options, next) {
   var redisClient = redisClientFactory(redis, options);
 
   /**
-   * error handler for errors after initial connection has been established 
+   * error handler for errors after initial connection has been established
    * @param {Error} err is the error thrown
    * @return {null}
    */
@@ -23,14 +23,14 @@ exports.register = function (server, options, next) {
     next(err);
     redisClient.end();
   };
-  
+
   redisClient.on('error', initialErrorHandler);
 
   redisClient.on("ready", function(){
     server.log([ 'hapi-redis', 'info' ], 'redisClient connection created');
-    
+
     // change the error handler to simply log errors
-    redisClient.removeListener('error', initialErrorHandler); 
+    redisClient.removeListener('error', initialErrorHandler);
     redisClient.on('error', defaultErrorHandler);
     next();
   });
